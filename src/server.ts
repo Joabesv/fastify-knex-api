561f8cec86e02685a9f3a5879be78373a7edeb8f
Joabesv <joabevarjao123@gmail.com>
2023-02-14 11:43:30 -0300

    chore<lint>: implement eslint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const app = fastify();
 
 app.get('/', () => {
-  return 'hello man'
-})
+  return 'hello man';
+});
 
 app.listen({ port: 5000 }).then(() => {
-  console.log('pai ta de pé')
-})
+  console.log('pai ta de pé');
+});
