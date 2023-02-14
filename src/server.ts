1f93c45624188fca25e83262e9c10088fae1f911
Joabesv <joabevarjao123@gmail.com>
2023-02-14 11:29:52 -0300

    chore<init>: start server

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get('/', () => {
+  return 'hello man'
+})
+
+app.listen({ port: 5000 }).then(() => {
+  console.log('pai ta de pé')
+})
